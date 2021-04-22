5cd5e0ca311c06be2d2e6a875e18fea898c50219
Andres9888 <Andres9888@Gmail.com>
2021-04-22 12:52:21 -0400

    -Add Min Size Button with Functionality

--- client/src/DataGrid.tsx
+++ client/src/DataGrid.tsx
@@ -1,47 +1,49 @@
-import React from "react";
+import React from 'react'
 
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import Chip from "@material-ui/core/Chip";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from '@material-ui/core/styles';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import SubdirectoryArrowRightIcon from '@material-ui/icons/SubdirectoryArrowRight';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Chip from '@material-ui/core/Chip'
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TablePagination from '@material-ui/core/TablePagination'
+import TableRow from '@material-ui/core/TableRow'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import SubdirectoryArrowRightIcon from '@material-ui/icons/SubdirectoryArrowRight'
 
-
-import { useListEntriesQuery } from "./generated-api";
+import { useListEntriesQuery } from './generated-api'
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
-  },
-});
+    minWidth: 650
+  }
+})
 
-function DataGrid() {
-  const classes = useStyles();
-  const [sizeGt, setSizeGt] = React.useState(200);
-  const [page, setPage] = React.useState(1);
+function DataGrid () {
+  const classes = useStyles()
+  const [sizeGt, setSizeGt] = React.useState(200)
+  const [sizeLt, setSizeLt] = React.useState(200)
+
+  const [page, setPage] = React.useState(1)
   const [currentPath, setCurrentPath] = React.useState('/')
-  const [history, updateHistory] = React.useState<{ id: string, path: string }[]>(
-    [{
+  const [history, updateHistory] = React.useState<
+    { id: string; path: string }[]
+  >([
+    {
       id: '/',
-      path: '/',
-    }]
-  )
+      path: '/'
+    }
+  ])
   const { data, loading, error } = useListEntriesQuery({
-    variables: { 
-      path: currentPath, 
-      page, 
+    variables: {
+      path: currentPath,
+      page,
       where: {
         /**
          * File Size
@@ -51,31 +53,32 @@ function DataGrid() {
          */
         // size_gt: sizeGt, // Int
         // size_lt: Int,
-          size_gt: sizeGt
+        size_gt: sizeGt,
+        size_lt: sizeLt
         /**
          * Entry Name Contains
          * @name name_contains an entry "name" text value to search on
          */
         // name_contains: String,
-        
+
         /**
          * Type Equals
          * @name type_eq Exact match for Entry type
          */
         // type_eq: "Directory" | "File",
       }
-    },
-  });
+    }
+  })
 
   React.useEffect(() => {
     setCurrentPath(history[history.length - 1].path)
   }, [history])
 
   const rows = React.useMemo(() => {
-    const dataRows = data?.listEntries?.entries ?? [] as any
+    const dataRows = data?.listEntries?.entries ?? ([] as any)
 
     return [
-      ...(history.length > 1 
+      ...(history.length > 1
         ? [
             {
               id: history[history.length - 2].id,
@@ -85,26 +88,29 @@ function DataGrid() {
             }
           ]
         : []),
-      ...dataRows,
+      ...dataRows
     ]
   }, [history.length, data?.listEntries?.entries])
 
   const rowCount = React.useMemo(() => {
-    const totalUpDirRows = currentPath === '/' 
-      ? 0 
-      : (data?.listEntries?.pagination.pageCount ?? 0) * 1
+    const totalUpDirRows =
+      currentPath === '/'
+        ? 0
+        : (data?.listEntries?.pagination.pageCount ?? 0) * 1
     const totalRowsFromServer = data?.listEntries?.pagination.totalRows ?? 0
-    return  totalRowsFromServer + totalUpDirRows
+    return totalRowsFromServer + totalUpDirRows
   }, [
-    data?.listEntries?.pagination.pageCount, 
+    data?.listEntries?.pagination.pageCount,
     data?.listEntries?.pagination.totalRows
   ])
 
   const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage + 1);
-  };
-
-  const handleDelete = () => {
+    setPage(newPage + 1)
+  }
+  const resetLt = () => {
+    setSizeLt(0)
+  }
+  const resetGt = () => {
     setSizeGt(0)
   }
   if (loading) {
@@ -114,31 +120,61 @@ function DataGrid() {
     return <h1>error</h1>
   }
   return (
-    <Box display="flex" height="100%">
+    <Box display='flex' height='100%'>
       <Box flexGrow={1}>
         <Paper>
           <Toolbar>
-            <Box display="flex" alignItems="center" justifyContent="space-between" width="100%">
-              <Typography variant="h4">File Browser</Typography>
+            <Box
+              display='flex'
+              alignItems='center'
+              justifyContent='space-between'
+              width='100%'
+            >
+              <Typography variant='h4'>File Browser</Typography>
               <Box>
-                <Chip 
-                  color="primary" 
-                  onDelete={handleDelete} 
+                <Chip
+                  color='primary'
+                  onDelete={resetLt}
                   label={
                     <Box>
-                      <strong>File Size &gt;</strong>
-                      <input 
-                        onChange={(e) => setSizeGt(Number(e.currentTarget.value))} 
-                        type="number"
+                      <strong>Max File Size &lt;</strong>
+                      <input
+                        onChange={e => setSizeLt(Number(e.currentTarget.value))}
+                        type='number'
+                        value={sizeLt}
+                        style={{
+                          marginLeft: 8,
+                          background: 'transparent',
+                          color: 'white',
+                          border: 'none',
+                          width: 80
+                        }}
+                        placeholder='Filter Value'
+                      />
+                    </Box>
+                  }
+                />
+              </Box>
+              <Box>
+                <Chip
+                  color='primary'
+                  onDelete={resetGt}
+                  label={
+                    <Box>
+                      <strong>Min File Size &gt;</strong>
+                      <input
+                        onChange={e => setSizeGt(Number(e.currentTarget.value))}
+                        type='number'
                         value={sizeGt}
                         style={{
                           marginLeft: 8,
                           background: 'transparent',
                           color: 'white',
                           border: 'none',
-                          width: 80,
+                          width: 80
                         }}
-                      placeholder="Filter Value"/>
+                        placeholder='Filter Value'
+                      />
                     </Box>
                   }
                 />
@@ -146,35 +182,42 @@ function DataGrid() {
             </Box>
           </Toolbar>
           <TableContainer>
-            <Table className={classes.table} size="small" aria-label="a dense table">
+            <Table
+              className={classes.table}
+              size='small'
+              aria-label='a dense table'
+            >
               <TableHead>
                 <TableRow>
                   <TableCell>Path</TableCell>
-                  <TableCell align="right">Name</TableCell>
-                  <TableCell align="right">Type</TableCell>
-                  <TableCell align="right">Size</TableCell>
+                  <TableCell align='right'>Name</TableCell>
+                  <TableCell align='right'>Type</TableCell>
+                  <TableCell align='right'>Size</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows.map(({path, __typename, name, size, id }) => {
+                {rows.map(({ path, __typename, name, size, id }) => {
                   const isUpDir = __typename === 'UP_DIR'
                   return (
                     <TableRow key={id}>
-                      <TableCell component="th" scope="row">
+                      <TableCell component='th' scope='row'>
                         <Button
-                          color="primary"
+                          color='primary'
                           disabled={__typename === 'File'}
-                          startIcon={isUpDir 
-                            ? (<MoreHorizIcon />)
-                            : (__typename === 'File' ? null : <SubdirectoryArrowRightIcon />)
+                          startIcon={
+                            isUpDir ? (
+                              <MoreHorizIcon />
+                            ) : __typename === 'File' ? null : (
+                              <SubdirectoryArrowRightIcon />
+                            )
                           }
                           onClick={() => {
-                            updateHistory((h) => {
-                              if (isUpDir && h.length > 1) {                  
+                            updateHistory(h => {
+                              if (isUpDir && h.length > 1) {
                                 setPage(1)
                                 return [...h.splice(0, h.length - 1)]
                               } else {
-                                return ([...h, { id: path, path }])
+                                return [...h, { id: path, path }]
                               }
                             })
                           }}
@@ -182,17 +225,24 @@ function DataGrid() {
                           {!isUpDir ? path : ''}
                         </Button>
                       </TableCell>
-                      <TableCell align="right">{isUpDir ? '_' : name}</TableCell>
-                      <TableCell align="right">{isUpDir ? '_' : __typename}</TableCell>
-                      <TableCell align="right">{isUpDir ? '_' : size}</TableCell>
+                      <TableCell align='right'>
+                        {isUpDir ? '_' : name}
+                      </TableCell>
+                      <TableCell align='right'>
+                        {isUpDir ? '_' : __typename}
+                      </TableCell>
+                      <TableCell align='right'>
+                        {isUpDir ? '_' : size}
+                      </TableCell>
                     </TableRow>
-                )})}
+                  )
+                })}
               </TableBody>
             </Table>
           </TableContainer>
           <TablePagination
             rowsPerPageOptions={[]}
-            component="div"
+            component='div'
             count={rowCount}
             rowsPerPage={25}
             page={page - 1}
@@ -201,7 +251,7 @@ function DataGrid() {
         </Paper>
       </Box>
     </Box>
-  );
+  )
 }
 
-export default DataGrid;
+export default DataGrid
